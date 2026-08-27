be463da08b2a172beccb49d2a2678f8d035a557e
William Moreno <graficowm@gmail.com>
2026-08-27 11:03:35 -0500

    feat(WIL-79): extraer panel de branding

---
+++ frontend/src/components/AdminBrandingPanel.stories.js
@@ -0,0 +1,3 @@
+import AdminBrandingPanel from './AdminBrandingPanel.vue'
+export default { title: 'Components/AdminBrandingPanel', component: AdminBrandingPanel }
+export const Default = { args: { showBrand: true, showQr: true, qrSize: 'M', bannerText: 'Bienvenidos' } }
