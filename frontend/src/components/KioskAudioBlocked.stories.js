c479928e4929b951d8f44a3751659fbd39f98b81
William Moreno <graficowm@gmail.com>
2026-08-27 10:29:50 -0500

    feat(kiosk): extract audio blocked component

---
+++ frontend/src/components/KioskAudioBlocked.stories.js
@@ -0,0 +1,4 @@
+import KioskAudioBlocked from './KioskAudioBlocked.vue'
+
+export default { title: 'Components/KioskAudioBlocked', component: KioskAudioBlocked, parameters: { layout: 'fullscreen' } }
+export const Blocked = {}
